39b848fcfaf7a9452b91b3fcd120bd298001b3d8
biqontie <awesome.abaildaev@yandex.kz>
2022-07-31 17:49:34 +0600

    first

---
+++ pkg/handler/handler.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"todo/pkg/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			item := lists.Group(":id/item")
+			{
+				item.POST("/", h.createItem)
+				item.GET("/", h.getAllItems)
+				item.GET("/:item_id", h.getItemById)
+				item.PUT("/:item_id", h.updateItem)
+				item.DELETE("/:item_id", h.deleteItem)
+
+			}
+		}
+	}
+	return router
+
+}
